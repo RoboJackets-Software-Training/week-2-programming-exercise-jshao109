66d59a08c10c1eb398a4efacd0e4854cc3474208
repl.it user <replituser@example.com>
2020-09-28 20:46:27 +0000

    Completed exercise 2.1

--- exercise_2_1/exercise_2_1.cpp
+++ exercise_2_1/exercise_2_1.cpp
@@ -6,22 +6,7 @@ int main() {
 
   std::vector<double> vec1 = {1,2,3,4,5,6,7};
   std::vector<double> vec2 = {2,5,6};
-  std::vector<double> result1 = applyConvolution(vec1, vec2, true);
-  std::vector<double> result2 = applyConvolution(vec1, vec2, false);
-  // Prints first convolution vector
-  std::cout << "{" << result1[0];
-  for (int i = 1; i < result1.size(); i++) 
-  {
-    std::cout << ", " << result1[i];
-  }
-  std::cout << "}\n";
-  // Prints second convolution vector
-  std::cout << "{" << result2[0];
-  for (int i = 1; i < result2.size(); i++) 
-  {
-    std::cout << ", " << result2[i];
-  }
-  std::cout << "}";
-
-  
+  printVector(applyConvolution(vec1, vec2, true));
+  printVector(applyConvolution(vec1, vec2, false));
+ 
 }
